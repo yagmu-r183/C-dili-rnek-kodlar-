f07b2b5f3e6c3a3409eacfc90f81d48732acd584
yagmu-r183 <y8819038@gmail.com>
2025-06-13 21:13:07 +0300

    Add files via upload

---
+++ calissma2_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("***maas hesabi***");
+	printf("\n\n");
+	float maas,yenimaas;
+	printf("maasinizi giriniz:");
+	scanf("%f",&maas);
+	yenimaas=maas*112/100;
+	printf("yeni maas degeri:%f",yenimaas);
+}
